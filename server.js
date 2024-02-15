67f4c405578a3b1abdaf90c2b1be3fbf5999514b
ABojo <Alex.bojorquez2598@gmail.com>
2024-02-14 19:48:20 -0500

    Setup the server's user route
    I setup the route that will be responsible for providing the user data
    to the front-end. I installed Octokit, GitHub's official Node.js SDK, and
    used it to fetch information about users. After the information has been
    received, either a success of an error message will be returned to the
    client.

--- server.js
+++ server.js
@@ -1,9 +1,49 @@
 require("dotenv").config();
 const express = require("express");
+const { Octokit } = require("octokit");
+
+const octokit = new Octokit({
+  auth: process.env.GITHUB_TOKEN,
+});
 
 const app = express();
 
 app.use(express.static("dist"));
+
+app.get("/api/user/:username", async (req, res) => {
+  let responseData = {};
+
+  try {
+    const { data } = await octokit.request("GET /users/{username}", { username: req.params.username });
+
+    responseData = {
+      status: "success",
+      data: {
+        username: data.login,
+        name: data.name,
+        bio: data.bio,
+        joinDate: data.created_at,
+        repos: data.public_repos,
+        following: data.following,
+        followers: data.followers,
+        twitter: data.twitter_username,
+        location: data.location,
+        company: data.company,
+        website: data.blog,
+      },
+    };
+  } catch (err) {
+    responseData = { status: "error", code: err.status };
+
+    if (err.status === 404) {
+      responseData = { ...responseData, message: "Sorry, that user could not be found!" };
+    } else {
+      responseData = { ...responseData, message: "Sorry, an error has occured!" };
+    }
+  }
+
+  res.json(responseData);
+});
 
 app.listen(process.env.PORT || 8080, () => {
   console.log("Server is listening for incoming requests!");
